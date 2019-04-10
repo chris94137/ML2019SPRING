313732944e6055131727d990d2bf4f141fa73bd0
Chris Huang <chris100253@gmail.com>
2019-04-10 23:36:16 +0800

    hw4 py

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -131,10 +131,11 @@ if __name__ == '__main__':
     for i in range(7):
         image = [images[i]] * 3
         image = np.concatenate(image, axis = 2)
-        # print(np.squeeze(model.predict(image[:, :, 0].reshape(-1, 48, 48, 1))))
+        np.random.seed(16)
         explanation = explainer.explain_instance(image, predict_, labels=(i, ), top_labels=None, hide_color=0, num_samples=1000)
         temp, mask = explanation.get_image_and_mask(i, positive_only=True, num_features=1000, hide_rest=True)
         plt.imsave(out_path + 'fig3_' + str(i) + '.jpg', mark_boundaries(temp / 2 + 0.5, mask))
+
 
     # Q4
     # bad_data = [x_train[i] for i in delete_list]
