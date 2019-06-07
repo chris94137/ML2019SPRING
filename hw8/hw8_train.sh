ca846c9a2a9415b44eeff8b9e44bcd183a95263f
Chris Huang <chris100253@gmail.com>
2019-06-07 13:38:39 +0800

    sh

---
+++ hw8/hw8_train.sh
@@ -0,0 +1,6 @@
+wget 'https://github.com/chris94137/ML-model/releases/download/0.0.1/best_0.69155ensemble2.h5'
+wget 'https://github.com/chris94137/ML-model/releases/download/0.0.1/best_0.68013ensemble3.h5'
+wget 'https://github.com/chris94137/ML-model/releases/download/0.0.1/best_ensemble1.h5'
+wget 'https://github.com/chris94137/ML-model/releases/download/0.0.1/best_ensemble5.h5'
+wget 'https://github.com/chris94137/ML-model/releases/download/0.0.1/best_0.69072ensemble4.h5'
+python hw8_train_kd.py $1 . --epoch=100
